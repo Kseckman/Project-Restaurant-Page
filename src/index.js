482b453e46656ecfc7813616f013639b52afcf07
kseckman <kseckm1@gmail.com>
2025-02-25 21:27:19 -0500

    wwebpack install complete

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+// import odinImage from "./odin.png";----example for images
+   
+// const image = document.createElement("img");
+// image.src = odinImage;
+   
+// document.body.appendChild(image);
+
+
+console.log(greeting);
