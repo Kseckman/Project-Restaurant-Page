2763715b9484089adac44cbc79a1fa694084709b
kseckman <kseckm1@gmail.com>
2025-03-04 16:31:52 -0500

    functionality complete

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import "./styles.css";
 import createHome from "./home.js";
 import createAbout from "./about.js";
 import createMenu from "./menu.js";
-// import odinImage from "./odin.png";---example for images
-   
-// const image = document.createElement("img");
-// image.src = odinImage;
-   
-// document.body.appendChild(image);
+
 const homePage = document.querySelector('#home')
 const aboutPage = document.querySelector('#about')
 const menuPage = document.querySelector('#menu')
 const content = document.querySelector('.content')
-let currentPage = 'home';
 
 const toggleNav = ()=>{
    const navBtns = document.querySelectorAll('.navbtn')
@@ -24,36 +18,25 @@ const toggleNav = ()=>{
 }
 
 homePage.addEventListener('click', ()=>{
-    if(currentPage === 'home') return
-
-    content.innerHTML = ''
+    content.innerHTML = '';
     createHome()
-    currentPage = 'home';
     toggleNav()
-    homePage.classList.toggle('bold')
+    home.classList.toggle('bold')
 })
 
 aboutPage.addEventListener('click', ()=>{
-    if(currentPage === 'about') return
     content.innerHTML = '';
-    createAbout()
-    currentPage = 'about';
-    console.log(currentPage)
+    createAbout()   
     toggleNav()
-    aboutPage.classList.toggle('bold')
+    about.classList.toggle('bold')
 })
 
 menuPage.addEventListener('click', ()=>{
-    if(currentPage === 'menu') return
     content.innerHTML = '';
-    createMenu()
-    currentPage = 'menu';
-    console.log(currentPage)
+    createMenu()    
     toggleNav()
     menu.classList.toggle('bold')
 })
 
-
 homePage.classList.toggle('bold')
-console.log(currentPage)
 createHome()
